41a200bb26396f180e1e0b7d89741af4b6585f02
Mahesh <chaitanyadarlapudi@gmail.com>
2025-09-15 10:53:49 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TweetsModule } from './tweets/tweet.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
   imports: [UsersModule, TweetsModule],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { retry } from 'rxjs';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return ' This get user get requrest';
+  }
+
+  @Post()
+  createUser() {
+    return 'this post user requrest';
+  }
+}
